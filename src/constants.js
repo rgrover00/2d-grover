2aa4d264cd4b9081a115c588e9876c99f2ec93f4
Ron Jon <rgrover00@gmail.com>
2024-12-24 17:24:10 -0500

    updated information

--- src/constants.js
+++ src/constants.js
@@ -1,35 +1,33 @@
 export const scaleFactor = 4;
 export const dialogueData = {
-  pc: `This is my Desktop PC.
+  pc: `This is my desktop PC.
 
-    I just like programming.
+    It’s my go-to spot for programming, tinkering with code, and bringing ideas to life. Whether I’m experimenting with new frameworks, self-hosting web applications, or optimizing workflows, this machine handles it all. 
 
-    Here is my <a href="https://github.com/rgrover00" target="_blank">Github</a>!`,
+    Want to see what I’ve been working on? Check out my <a href="https://github.com/rgrover00" target="_blank">GitHub</a>!`,
 
-  "cs-degree": `This is my CS Networking Degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+"cs-degree": "This is my Computer Science Networking Degree. I proudly hung it on the wall because it represents years of hard work and dedication. While the program was highly theoretical, it gave me a strong foundation in systems, networking, and problem-solving that I use daily in my career.",
 
-  "sofa-table": `That's my sofa. I like pretend to relax while watching my two little kiddos. 
-  
-  I also like to self host web applications and workout in my garage gym. Check out <a href="https://ronniegrover.com" target="_blank">my personal website</a>!`,
+  "sofa-table": "That's my sofa. I like pretend to relax while watching my two little kiddos. \n\n  I also like to self host web applications and workout in my garage gym. Check out <a href=\"https://ronniegrover.com\" target=\"_blank\">my personal website</a>!",
 
   tv: 
   `That's my TV. I watch tech youtubers and twitch a lot recently like:
 
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.twitch.tv/nmplol" target="_blank">nmplol</a>!`,
 
-  bed: `This where I pretend to sleep.`,
+  bed: "This is where I try and pretend to get some reset and relaxation.",
 
   resume: `This is my desk and on it is my current resume. 
-  <a href="https://github.com/rgrover00/resume/blob/master/RonnieGrover-Resume.pdf" target="_blank">Take a look?</a>
-  Contact me at rkg@ronniegrover.com if you have any interesting job opportunities!`,
+  <a href="https://resume.ronniegrover.com/" target="_blank">Take a look?</a>
+  Contact me at rkg@ronniegrover.com`,
 
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. 
   
   This was created with Kaboom.js and by following a tutorial created by <a href="https://github.com/jslegenddev" target="_blank">jslegenddev</a>`,
 
-  library: `There are a lot of programming books on my shelves..I probably only read one of them. `,
+  library: "There are a lot of programming books on my shelves..I probably only read one of them. ",
 
-  exit: `look for what you can't see`,
+  exit: "look for what you can't see",
   
 };
