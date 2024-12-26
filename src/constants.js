111e5eaa97fcb1625cb1723797a029f077c56bbe
Ron Jon <rgrover00@gmail.com>
2024-12-26 13:52:21 -0500

    fixed text

--- src/constants.js
+++ src/constants.js
@@ -3,16 +3,16 @@ export const dialogueData = {
   pc: `Welcome to my PC—my hub for all things tech (and occasionally trying to game when life allows). Here, I solve networking problems, pretend to code, and work on self-hosted projects.
   Curious about my work? Check out my <a href="https://github.com/rgrover00" target="_blank">GitHub</a>!`,
 
-  "cs-degree": "Alongside my CIT Networking Degree, my wall features certifications that highlight my expertise in networking, cloud computing, and IT management, including in Extreme Networks, Google , and AWS. Each reflects my dedication to growth and staying at the forefront of technology. Explore all my certifications on my <a href=\"https://www.credly.com/users/ronniegrover\" target=\"_blank\">Credly profile</a>! ",
+  "cs-degree": "Alongside my CIT Networking Degree, my wall features certifications that highlight my expertise in networking, cloud computing, and IT management, including in Extreme Networks,Google , and AWS. Each reflects my dedication to growth and staying at the forefront of technology. Explore all my certifications on <a href=\"https://www.credly.com/users/ronniegrover\" target=\"_blank\">Credly</a>! ",
 
-  "sofa-table": "This is my sofa, where I pretend to relax while spending time with my wife, Stinabean, and keeping up with our three little adventurers—Ayla (4), Bryson (2), and baby Everly. It’s also where I brainstorm self-hosted web projects or unwind after a garage gym workout. Curious about my work? Check out <a href=\"https://ronniegrover.com\" target=\"_blank\">my personal website</a>!  ",
+  "sofa-table": "This is my sofa, where I pretend to relax while spending time with my wife, Stinabean, and keeping up with our three little adventurers—Ayla (4), Bryson (2), and baby Everly. It’s also where I brainstorm self-hosted web projects or unwind after a garage gym workout. Curious about my work? Check out <a href=\"https://ronniegrover.com\" target=\"_blank\">website</a>!  ",
 
   tv: 
   `This is our family TV with three little ones it’s become more of a learning tool than an entertainment hub. From colorful animations that teach letters and numbers to fun science experiments and music, it’s all about keeping the kids engaged while sparking their curiosity.
 
   Occasionally, when the chaos settles, I’ll use it to catch up on tech YouTubers or Twitch streams—but let’s be honest, the kids run the programming most of the time!`,
 
-  bed: "This is my relaxation chair—or at least, that’s what I call it. In reality, it’s where I pretend to relax while keeping an eye on my three little ones: Ayla (4), Bryson (2), and baby Everly. Between coding projects, managing systems, and the wonderful chaos of parenting, this spot serves as my unofficial command center for multitasking. On rare occasions, I actually get to sit here uninterrupted, brainstorming new ideas, catching up on tech content, or just taking a breather. But let’s be honest, with three kids, relaxation is more of a concept than a reality!",
+  bed: "This is my relaxation corner, that’s what I call it. Between coding projects, managing systems, and the wonderful chaos of parenting, this spot serves as my unofficial command center for multitasking. On rare occasions, I actually get to sit here uninterrupted, brainstorming new ideas, catching up on tech content, or just taking a breather. But let’s be honest, with three kids, relaxation is more of a concept than a reality!",
 
   resume: `This is my desk and on it is my current resume. 
   <a href="https://resume.ronniegrover.com/#resume" target="_blank">Take a look?</a>
