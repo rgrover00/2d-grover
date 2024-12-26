5f48e6f27ed92d72e6f97a17a9c7410ff2a7cf00
Ron Jon <rgrover00@gmail.com>
2024-12-26 13:36:33 -0500

    shorten descriptions

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,16 @@
 export const scaleFactor = 4;
 export const dialogueData = {
   pc: `Welcome to my PC—my hub for all things tech (and occasionally trying to game when life allows). Here, I solve networking problems, code, and build with frameworks like Next.js, React, and Node.js to create efficient, user-friendly systems.
+  When I’m not diving into web development, self-hosting projects, or sneaking in some gaming, I’m optimizing workflows and exploring new ways to make technology work better. Curious about my work? Check out my <a href="https://github.com/rgrover00" target="_blank">GitHub</a>!`,
 
-When I’m not diving into web development, self-hosting projects, or sneaking in some gaming, I’m optimizing workflows and exploring new ways to make technology work better. Curious about my work? Check out my <a href="https://github.com/rgrover00" target="_blank">GitHub</a>!`,
+  "cs-degree": "Alongside my CIT Networking Degree, my wall features certifications that highlight my expertise in networking, cloud computing, and IT management, including Extreme Networks, Google Workspace, and AWS Cloud Practitioner. Each reflects my dedication to growth and staying at the forefront of technology. Explore all my certifications on my <a href=\"https://www.credly.com/users/ronniegrover\" target=\"_blank\">Credly profile</a>! ",
 
-"cs-degree": "Alongside my CIT Networking Degree, my wall features certifications that highlight my expertise in networking, cloud computing, and IT management, including Extreme Networks, Google Workspace, and AWS Cloud Practitioner. Each reflects my dedication to growth and staying at the forefront of technology. Explore all my certifications on my <a href=\"https://www.credly.com/users/ronniegrover\" target=\"_blank\">Credly profile</a>! ",
-
-"sofa-table": "This is my sofa, where I pretend to relax while spending time with my wife, Stinabean, and keeping up with our three little adventurers—Ayla (4), Bryson (2), and baby Everly. It’s also where I brainstorm self-hosted web projects or unwind after a garage gym workout. Curious about my work? Check out <a href=\"https://ronniegrover.com\" target=\"_blank\">my personal website</a>!  ",
+  "sofa-table": "This is my sofa, where I pretend to relax while spending time with my wife, Stinabean, and keeping up with our three little adventurers—Ayla (4), Bryson (2), and baby Everly. It’s also where I brainstorm self-hosted web projects or unwind after a garage gym workout. Curious about my work? Check out <a href=\"https://ronniegrover.com\" target=\"_blank\">my personal website</a>!  ",
 
   tv: 
-  `This is our family TV, which mostly plays children’s educational videos these days. With three little ones—Ayla (4), Bryson (2), and baby Everly—it’s become more of a learning tool than an entertainment hub. From colorful animations that teach letters and numbers to fun science experiments and music, it’s all about keeping the kids engaged while sparking their curiosity.
+  `This is our family TV with three little ones it’s become more of a learning tool than an entertainment hub. From colorful animations that teach letters and numbers to fun science experiments and music, it’s all about keeping the kids engaged while sparking their curiosity.
 
-Occasionally, when the chaos settles, I’ll use it to catch up on tech YouTubers or Twitch streams—but let’s be honest, the kids run the programming most of the time!`,
+  Occasionally, when the chaos settles, I’ll use it to catch up on tech YouTubers or Twitch streams—but let’s be honest, the kids run the programming most of the time!`,
 
   bed: "This is my relaxation chair—or at least, that’s what I call it. In reality, it’s where I pretend to relax while keeping an eye on my three little ones: Ayla (4), Bryson (2), and baby Everly. Between coding projects, managing systems, and the wonderful chaos of parenting, this spot serves as my unofficial command center for multitasking. On rare occasions, I actually get to sit here uninterrupted, brainstorming new ideas, catching up on tech content, or just taking a breather. But let’s be honest, with three kids, relaxation is more of a concept than a reality!",
 
@@ -22,7 +21,7 @@ Occasionally, when the chaos settles, I’ll use it to catch up on tech YouTuber
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
 
-  library: "This is my bookshelf, home to a collection of programming, networking, and tech-related books. While I’ll admit I haven’t read them all cover-to-cover (who has the time with three kids running around?), they serve as a constant source of inspiration and reference. From foundational concepts in computer science to modern frameworks and cloud architecture, these books represent my commitment to lifelong learning and staying ahead in the ever-evolving tech world. Whether I’m brushing up on a topic, troubleshooting a complex issue, or just sparking new ideas, this bookshelf is a reminder that knowledge is always within reach—if not always within my current schedule!",
+  library: "This is my bookshelf, packed with programming, networking, and tech books. While I haven’t read them all (three kids keep me busy!), they’re a constant source of inspiration and reference. From computer science basics to modern frameworks, they reflect my commitment to lifelong learning—always within reach, even if not always within my schedule!",
 
   exit: "look for what you can't see",
   
